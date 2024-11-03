2352567a75c17a3518ae21a00249aabc9ada81cc
Morten Kromberg <mkrom@dyalog.com>
2024-11-03 16:51:26 +0100

    Checkpoint at November 3rd

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Dyalog\Dyalog APL-64 19.0 Unicode\dyalog.exe" LOAD="\devt\owum\APLSource" LX="Run 'Browser'" DYALOG_NETCORE=1
+"C:\Program Files\Dyalog\Dyalog APL-64 19.0 Unicode\dyalog.exe" LOAD="\devt\owum\APLSource" LX="Run 'Browser'" DYALOG_NETCORE=0
